58bee2074f488df3106dba21ab1dab57fbd290ea
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2023-10-29 22:23:26 +0530

    Day 3 of learning JS
    Learnt typec_onversion

---
+++ Basics_js/typeconversion.js
@@ -0,0 +1,60 @@
+// let age="33"
+// console.log(typeof(age));
+
+// let ageinNumbers = Number(age)
+// console.log(ageinNumbers);
+// console.log(typeof(ageinNumbers));
+
+/*
+let name="abc"
+console.log(typeof(name));
+
+let nameinNumber = Number(name)
+console.log(nameinNumber);
+console.log(typeof(nameinNumber));
+
+
+let temp = null
+console.log(typeof(temp));
+
+let tempinNumber = Number(temp)
+console.log(tempinNumber);
+console.log(typeof(tempinNumber));
+*/
+
+/*
+let payment = true;
+console.log(typeof(payment));
+
+let paymentinNumber = Number(payment)
+console.log(paymentinNumber);
+console.log(typeof(paymentinNumber));
+
+"33" => 33
+"33aa" => NaN
+true =>1 and false =>0
+*/
+
+/*
+let num= 1;
+let numberinBool= Boolean(num);
+console.log(numberinBool);
+
+1 => true
+*/
+
+/*
+let num= "";
+let numberinBool= Boolean(num);
+console.log(numberinBool);
+
+"" => false
+*/
+ 
+/*
+let num= "shree";
+let numberinBool= Boolean(num);
+console.log(numberinBool);
+
+"shree" => true
+*/
